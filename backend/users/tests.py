540dfb0a2843ab6c4b5722c8c66975e5a2a75a44
Rufus <apdubrov@gmail.com>
2022-05-02 21:46:02 +0300

    homework 8

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
 # Create your tests here.
+import json
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from mixer.backend.django import mixer
+from .views import UserModelViewSet
+from .models import User
+
+
+class TestUserViewSet(TestCase):
+
+    def test_edit_admin(self):
+        user = User.objects.create(username='aaa', email='aaa@aa.com')
+        client = APIClient()
+        User.objects.create_superuser('admin', 'admin@admin.com', 'admin123456')
+        client.login(username='admin', password='admin123456')
+        response = client.put(f'/api/users/{user.id}/', {'username': 'Грин', 'email': 'ss@ss.com'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        user = User.objects.get(id=user.id)
+        self.assertEqual(user.username, 'Грин')
+        self.assertEqual(user.email, 'ss@ss.com')
+        client.logout()
+
+    def test_get_list_403(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/users/')
+        view = UserModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
